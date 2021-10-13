4adfd027b7b10de9c3e4c730e1a211a742dfdc61
Dan Herrera <whoahbot@bytewax.io>
2021-10-13 11:08:48 -0700

    Format with black.

--- ernie/ernie.py
+++ ernie/ernie.py
@@ -3,16 +3,16 @@ from transformers import pipeline
 
 from ernie.forms import AskErnie
 
-app = Flask(__name__, template_folder='../templates', static_folder="../static")
-app.config['SECRET_KEY'] = 'rubber duckie'
-unmasker = pipeline('fill-mask', model='distilbert-base-uncased')
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
+app.config["SECRET_KEY"] = "rubber duckie"
+unmasker = pipeline("fill-mask", model="distilbert-base-uncased")
+
 
 @app.route("/")
 def index():
     form = AskErnie()
-    return render_template(
-        "index.html", form=form
-    )
+    return render_template("index.html", form=form)
+
 
 @app.route("/ask", methods=["POST"])
 def ask():
--- tests/test_ernie.py
+++ tests/test_ernie.py
@@ -2,6 +2,7 @@ import pytest
 
 from ernie.ernie import app
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
@@ -9,7 +10,10 @@ def client():
 
 
 def test_ernie_results(client):
-    resp = client.post('/ask', data={"prompt": "You might wonder what a city kid like me is doing out here in the [MASK]?"})
+    resp = client.post(
+        "/ask",
+        data={
+            "prompt": "You might wonder what a city kid like me is doing out here in the [MASK]?"
+        },
+    )
     assert b"you might wonder what a city kid" in resp.data
-
-
